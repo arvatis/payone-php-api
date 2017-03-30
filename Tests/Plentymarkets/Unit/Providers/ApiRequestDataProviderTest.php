bbca3171b52c9473bf07ec1b65cfcb5ce6ba2d9b
Simon Heinen <heinen@arvatis.com>
2017-03-30 10:31:52 +0200

    add tests for request data retrieval

---
+++ Tests/Plentymarkets/Unit/Providers/ApiRequestDataProviderTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Payone\Tests\Unit\Providers;
+
+use Payone\Helper\PaymentHelper;
+use Payone\Providers\ApiRequestDataProvider;
+use Payone\Services\SessionStorageService;
+use Plenty\Modules\Account\Address\Contracts\AddressRepositoryContract;
+use Plenty\Modules\Basket\Models\Basket;
+use Plenty\Modules\Item\Item\Contracts\ItemRepositoryContract;
+use Plenty\Modules\Order\Shipping\Countries\Contracts\CountryRepositoryContract;
+use Plenty\Modules\Order\Shipping\Information\Contracts\ShippingInformationRepositoryContract;
+
+/**
+ * Class PaymentHelperTest
+ */
+class ApiRequestDataProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var ApiRequestDataProvider */
+    private $resource;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->resource = new ApiRequestDataProvider(
+            $this->createMock(PaymentHelper::class),
+            $this->createMock(AddressRepositoryContract::class),
+            $this->createMock(SessionStorageService::class),
+            $this->createMock(ItemRepositoryContract::class),
+            $this->createMock(CountryRepositoryContract::class),
+            $this->createMock(ShippingInformationRepositoryContract::class)
+        );
+    }
+
+    public function testDataIsAlwaysReturned()
+    {
+        $basket = $this->createMock(Basket::class);
+        $requestData = $this->resource->getPreAuthData($basket);
+
+        //print_r($requestData);
+
+        $this->assertArrayHasKey('basket', $requestData);
+        $this->assertArrayHasKey('basketItems', $requestData);
+        $this->assertArrayHasKey('shippingAddress', $requestData);
+        $this->assertArrayHasKey('country', $requestData);
+        $this->assertArrayHasKey('country', $requestData);
+    }
+
+}
