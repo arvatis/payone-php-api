d4dfbdb93cd4642a0fd0ace44eab8cb1a98c41a1
Simon Heinen <heinen@arvatis.com>
2018-04-05 11:58:10 +0200

    Revert "pass sequencenumber"
    This reverts commit a2fa44e

--- src/Request/Authorization/RequestFactory.php
+++ src/Request/Authorization/RequestFactory.php
@@ -81,20 +81,6 @@ class RequestFactory implements RequestFactoryContract
                     self::createUrls($data['redirect']),
                     $bankAccount
                 );
-            case PaymentTypes::PAYONE_ON_LINE_BANK_TRANSFER:
-                $bankAccountData = $data['bankAccount'];
-                $bankAccount = new BankAccount(
-                    $bankAccountData['country'],
-                    $bankAccountData['holder'],
-                    $bankAccountData['iban'],
-                    $bankAccountData['bic']
-                );
-
-                return new OnlineBankTransfer(
-                    $genericAuthRequest,
-                    self::createUrls($data['redirect']),
-                    $bankAccount
-                );
 
             case PaymentTypes::PAYONE_PAYDIREKT:
                 $customerAddressData = $data['shippingAddress'];
